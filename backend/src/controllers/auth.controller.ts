606f4b1fa84aa2f408f00fa2f85f29bd93039af5
arnobpaul46 <arnobpaulbd@gmail.com>
2026-07-11 15:27:19 +0600

    login api created and login route tested successfully

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,12 +1,10 @@
-// ============================================
-// ধাপ ৪.১: রেজিস্টার কন্ট্রোলার (লজিক অংশ)
-// ============================================
-
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { User } from '../models/User.model';
-
+import jwt from 'jsonwebtoken';
+// ============================================
 // register user api
+// ============================================
 export const register = async (req: Request, res: Response) => {
   try {
     // get request body
@@ -59,6 +57,64 @@ export const register = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.error('Registration error:', error);
+    res.status(500).json({ message: 'Server error. Please try again later.' });
+  }
+};
+
+
+
+// ============================================
+// login user api
+// ============================================
+export const login = async (req: Request, res: Response) => {
+  try {
+    // get request body
+    const { email, password } = req.body;
+
+    // validation
+    if (!email) {
+      return res.status(400).json({ message: 'Email is required' });
+    }
+    if (!password) {
+      return res.status(400).json({ message: 'Password is required' });
+    }
+
+    // find user by email
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' }); // 401 = অননুমোদিত
+    }
+
+    // compare password with hashed password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+
+    const token = jwt.sign(
+      { 
+        id: user._id, 
+        role: user.role 
+      },
+      process.env.JWT_SECRET!, 
+      { expiresIn: process.env.JWT_EXPIRES_IN || '7d' } 
+    );
+
+    
+    res.status(200).json({
+      message: 'Login successful',
+      token,  
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        subscriptionStatus: user.subscriptionStatus, 
+      },
+    });
+  } catch (error) {
+    console.error('Login error:', error);
     res.status(500).json({ message: 'Server error. Please try again later.' });
   }
 };
