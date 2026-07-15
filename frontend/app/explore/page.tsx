423729a42893a6f5b38b17751d20f7c7ef1a3dfd
arnobpaul46 <arnobpaulbd@gmail.com>
2026-07-15 21:35:50 +0600

    vul hoiyec path create er jono and 9th try

--- frontend/app/explore/page.tsx
+++ frontend/app/explore/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { ExploreContent } from '@/components/ExploreContent';
+import { ExploreContent } from "@/components/home/ExploreContent";
 
 export default function ExplorePage() {
   return (
